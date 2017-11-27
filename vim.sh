6a52fc6d8d6c96998664c75e4120460df2537523
deve <deve_m@163.com>
2017-11-27 18:50:08 +0800

    vimsh
    vimsh

--- vim.sh
+++ vim.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.vim"
-
 [ -z "$REPO_PATH" ] && REPO_PATH="$HOME/.Dvim"
 [ -z "$REPO_URI" ] && REPO_URI="https://github.com/manjuist/Dvim.git"
-
 [ -z "$VUNDLE_PATH" ] && VUNDLE_PATH="$HOME/.vim/bundle/Vundle.vim"
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/VundleVim/Vundle.vim.git"
-
 [ -z "$FONT_PATH" ] && FONT_PATH="$HOME/.font"
 
-debug(){
-	echo $ret
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" -eq '0' ]; then
+        msg "\33[32m[✔]\33[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
 }
 
 sync_repo() {
@@ -32,97 +40,42 @@ sync_repo() {
     debug
 }
 
+install_plugins() {
+    local system_shell="$SHELL"
+    export SHELL='/bin/sh'
+
+    vim "+BundleInstall!" "+BundleClean" "+qall"
+
+    export SHELL="$system_shell"
+}
+
 copy_font() {
-    local source_font_path=$1
-    local target_font_path=$2
+    local source_font_path="$1"
+    local target_font_path="$2"
 
     cp "$source_font_path" "$target_path"
+}
+
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+    ret="$?"
+    debug
+}
+
+install_font(){
+    local font_path="$1"
+    cd font_path && mkfontscale && mkfontdir && fc-cache
 }
 
 sync_repo "$REPO_PATH" "$REPO_URI"
 sync_repo "$VUNDLE_PATH" "$VUNDLE_URI"
 copy_font "$REPO_PATH/*.ttf" "$FONT_PATH"
+install_plugins
 
-vim "+BundleInstall!" "+BundleClean" "+qall"
-##   Copyright 2014 Steve Francia
-##
-##   Licensed under the Apache License, Version 2.0 (the "License");
-##   you may not use this file except in compliance with the License.
-##   You may obtain a copy of the License at
-##
-##       http://www.apache.org/licenses/LICENSE-2.0
-##
-##   Unless required by applicable law or agreed to in writing, software
-##   distributed under the License is distributed on an "AS IS" BASIS,
-##   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-##   See the License for the specific language governing permissions and
-##   limitations under the License.
 #
-#############################  SETUP PARAMETERS
-#app_name='spf13-vim'
-#[ -z "$APP_PATH" ] && APP_PATH="$HOME/.spf13-vim-3"
-#[ -z "$REPO_URI" ] && REPO_URI='https://github.com/spf13/spf13-vim.git'
-#[ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
-#debug_mode='0'
-#fork_maintainer='0'
-#[ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
-#
-#############################  BASIC SETUP TOOLS
-#msg() {
-#    printf '%b\n' "$1" >&2
-#}
-#
-#success() {
-#    if [ "$ret" -eq '0' ]; then
-#        msg "\33[32m[✔]\33[0m ${1}${2}"
-#    fi
-#}
-#
-#error() {
-#    msg "\33[31m[✘]\33[0m ${1}${2}"
-#    exit 1
-#}
-#
-#debug() {
-#    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
-#        msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
-#    fi
-#}
-#
-#program_exists() {
-#    local ret='0'
-#    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
-#
-#    # fail on non-zero return value
-#    if [ "$ret" -ne 0 ]; then
-#        return 1
-#    fi
-#
-#    return 0
-#}
-#
-#program_must_exist() {
-#    program_exists $1
-#
-#    # throw error on non-zero return value
-#    if [ "$?" -ne 0 ]; then
-#        error "You must have '$1' installed to continue."
-#    fi
-#}
-#
-#variable_set() {
-#    if [ -z "$1" ]; then
-#        error "You must have your HOME environmental variable set to continue."
-#    fi
-#}
-#
-#lnif() {
-#    if [ -e "$1" ]; then
-#        ln -sf "$1" "$2"
-#    fi
-#    ret="$?"
-#    debug
-#}
 #
 ############################# SETUP FUNCTIONS
 #
@@ -212,6 +165,8 @@ vim "+BundleInstall!" "+BundleClean" "+qall"
 #        "+BundleClean" \
 #        "+qall"
 #
+#    local system_shell="$SHELL"
+#    export SHELL='/bin/sh'
 #    export SHELL="$system_shell"
 #
 #    success "Now updating/installing plugins using Vundle"
