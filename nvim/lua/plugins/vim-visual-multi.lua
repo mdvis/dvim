f4240f98d5aacccd125357702c6df6e13ec594c3
dewei.ma <xnmageis@outlook.com>
2026-04-28 14:49:57 +0800

    feat(vim-visual-multi): add lazy loading and keybinding documentation
    - Add VeryLazy event for deferred plugin loading
    - Document <C-n> keybinding for starting/adding cursors in normal and visual modes
    - Document <C-Down> keybinding for adding cursor down
    - Document <C-Up> keybinding for adding cursor up
    - Improves plugin startup performance while ensuring availability

--- nvim/lua/plugins/vim-visual-multi.lua
+++ nvim/lua/plugins/vim-visual-multi.lua
@@ -1,4 +1,10 @@
 return {
   "mg979/vim-visual-multi",
   branch = "master",
+  event = "VeryLazy", -- 延迟加载，但确保会被加载
+  keys = {
+    { "<C-n>", mode = { "n", "x" }, desc = "Start/Add cursor" },
+    { "<C-Down>", mode = { "n", "x" }, desc = "Add cursor down" },
+    { "<C-Up>", mode = { "n", "x" }, desc = "Add cursor up" },
+  },
 }
