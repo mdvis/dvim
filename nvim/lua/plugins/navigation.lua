67c85bdd8675844ecedbc5d77a37c6d3a1dd719e
dewei.ma <xnmageis@outlook.com>
2026-04-28 12:54:23 +0800

    fix(navigation): 修复 harpoon 列表持久化丢失问题
    使用 vim.fs.root 可靠获取 git root 作为存储 key，移除无效的
    harpoon.save 调用，开启 save_on_toggle 和 sync_on_ui_close 确保
    列表跨会话持久化。

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,69 +1,96 @@
 return {
-  {
-    "folke/flash.nvim",
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-      },
-      {
-        "S",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").treesitter()
-        end,
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter_search()
-        end,
-      },
+    {
+        "folke/flash.nvim",
+        keys = {
+            {
+                "s",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+            },
+            {
+                "S",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").treesitter()
+                end,
+            },
+            {
+                "r",
+                mode = "o",
+                function()
+                    require("flash").remote()
+                end,
+            },
+            {
+                "R",
+                mode = { "o", "x" },
+                function()
+                    require("flash").treesitter_search()
+                end,
+            },
+        },
+        opts = {
+            search = { mode = "exact" },
+        },
     },
-    opts = {
-      search = { mode = "exact" },
+    {
+        "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "xiyaowong/telescope-emoji.nvim",
+        },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({})
+            pcall(telescope.load_extension, "emoji")
+            pcall(telescope.load_extension, "fzf")
+        end,
     },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    cmd = "Telescope",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "xiyaowong/telescope-emoji.nvim",
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup()
+        end,
+        keys = {
+            {
+                "<leader>a",
+                function()
+                    local harpoon = require("harpoon")
+                    harpoon:list():add()
+                end,
+            },
+            {
+                "<C-e>",
+                function()
+                    local harpoon = require("harpoon")
+                    harpoon.ui:toggle_quick_menu(harpoon:list())
+                end,
+            },
+        },
+        settings = {
+            save_on_toggle = true,
+            sync_on_ui_close = true,
+            key = function()
+                return vim.loop.cwd()
+            end,
+        },
     },
-    config = function()
-      local telescope = require("telescope")
-      telescope.setup({})
-      pcall(telescope.load_extension, "emoji")
-      pcall(telescope.load_extension, "fzf")
-    end,
-  },
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    opts = {},
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    cmd = "Neotree",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        cmd = "Neotree",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {},
     },
-    opts = {},
-  },
 }
