b835225495034e301de042456da260e6a8a391d0
dewei.ma <xnmageis@outlook.com>
2026-04-28 18:17:41 +0800

    chore(nvim): consolidate treesitter-textobjects into main plugin config
    - Remove separate nvim-treesitter-textobjects plugin file
    - Integrate textobjects configuration directly into nvim-treesitter
    - Add textobjects as dependency in main treesitter plugin
    - Configure select keymaps for functions, classes, and parameters
    - Configure move keymaps for navigating between code objects
    - Configure swap keymaps for reordering parameters
    - Simplifies plugin management by consolidating related configurations

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
   event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
   opts = {
@@ -29,6 +30,53 @@ return {
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
+          ["]a"] = "@parameter.inner",
+        },
+        goto_next_end = {
+          ["]F"] = "@function.outer",
+          ["]C"] = "@class.outer",
+          ["]A"] = "@parameter.inner",
+        },
+        goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
+          ["[a"] = "@parameter.inner",
+        },
+        goto_previous_end = {
+          ["[F"] = "@function.outer",
+          ["[C"] = "@class.outer",
+          ["[A"] = "@parameter.inner",
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+    },
   },
   config = function(_, opts)
     require("nvim-treesitter").setup(opts)
