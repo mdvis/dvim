9cfa3a9e59769c751ac7501741a91cde069edb65
dewei.ma <xnmageis@outlook.com>
2026-04-28 11:37:17 +0800

    refactor(nvim): reorganize colorscheme plugins into ui config
    - Move catppuccin and tokyonight plugins from core.lua to ui.lua
    - Consolidate theme-related configurations in dedicated ui module
    - Maintain plugin priority and lazy-loading behavior

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -2,14 +2,4 @@ return {
   { "nvim-lua/plenary.nvim" },
   { "MunifTanjim/nui.nvim" },
   { "nvim-tree/nvim-web-devicons" },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
-  { "folke/tokyonight.nvim" },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -47,4 +47,14 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  { "folke/tokyonight.nvim" },
 }
