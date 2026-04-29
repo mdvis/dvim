2aaf7631752602c7bfd01ba96e9616583b236571
dewei.ma <xnmageis@outlook.com>
2026-04-29 18:21:43 +0800

    chore(comment): 移除不必要的配置选项

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,21 +1,4 @@
 return {
   "numToStr/Comment.nvim",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    padding = true,
-    sticky = true,
-    ignore = "^$",
-    toggler = {
-      line = "gcc",
-      block = "gbc",
-    },
-    opleader = {
-      line = "gc",
-      block = "gb",
-    },
-    mappings = {
-      basic = true,
-      extra = true,
-    },
-  },
+  opts = {}
 }
