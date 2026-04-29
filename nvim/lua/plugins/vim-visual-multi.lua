0fbf8d0c8882a14bff7780d566c40ab6bddb0ec9
dewei.ma <xnmageis@outlook.com>
2026-04-29 10:00:16 +0800

    chore(plugins): remove chinese comment from vim-visual-multi config
    - Remove Chinese comment from event configuration line
    - Simplify configuration by keeping only essential English documentation
    - Maintain consistent code style with rest of plugin configurations

--- nvim/lua/plugins/vim-visual-multi.lua
+++ nvim/lua/plugins/vim-visual-multi.lua
@@ -1,7 +1,7 @@
 return {
   "mg979/vim-visual-multi",
   branch = "master",
-  event = "VeryLazy", -- 延迟加载，但确保会被加载
+  event = "VeryLazy",
   keys = {
     { "<C-n>", mode = { "n", "x" }, desc = "Start/Add cursor" },
     { "<C-Down>", mode = { "n", "x" }, desc = "Add cursor down" },
