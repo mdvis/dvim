44ac7367c402ab628b9bc1100fa5c8fcc698d073
dewei.ma <xnmageis@outlook.com>
2026-04-29 10:00:42 +0800

    feat(nvim): migrate symbols-outline to outline.nvim plugin
    - Replace simrat39/symbols-outline.nvim with hedyhli/outline.nvim
    - Update command names from SymbolsOutline to Outline variants
    - Restructure configuration to use new outline.nvim option schema
    - Reorganize settings into outline_window, outline_items, preview_window, and providers sections
    - Remove Chinese comments and modernize configuration structure
    - Update keymaps with new actions (peek_location, goto_and_close, restore_location, down_and_jump, up_and_jump)
    - Migrate symbol icon definitions to nested icons structure
    - Improve window and preview behavior with updated configuration options

--- nvim/lua/plugins/symbols-outline.lua
+++ nvim/lua/plugins/symbols-outline.lua
@@ -1,45 +1,48 @@
 return {
-  "simrat39/symbols-outline.nvim",
-  cmd = { "SymbolsOutline", "SymbolsOutlineOpen", "SymbolsOutlineClose" },
+  "hedyhli/outline.nvim",
+  cmd = { "Outline", "OutlineOpen", "OutlineClose" },
   keys = {
-    { "<leader>so", "<cmd>SymbolsOutline<cr>", desc = "Toggle Symbols Outline" },
+    { "<leader>so", "<cmd>Outline<cr>", desc = "Toggle Symbols Outline" },
   },
   opts = {
-    -- 高亮当前符号
-    highlight_hovered_item = true,
-    -- 显示符号指南线
-    show_guides = true,
-    -- 自动预览
-    auto_preview = false,
-    -- 位置
-    position = "right",
-    -- 相对宽度
-    relative_width = true,
-    -- 宽度
-    width = 25,
-    -- 自动关闭
-    auto_close = false,
-    -- 显示数字
-    show_numbers = false,
-    -- 显示相对数字
-    show_relative_numbers = false,
-    -- 显示符号详情
-    show_symbol_details = true,
-    -- 预览背景高亮
-    preview_bg_highlight = "Pmenu",
-    -- 自动展开
-    autofold_depth = nil,
-    -- 自动展开深度
-    auto_unfold_hover = true,
-    -- 折叠标记
-    fold_markers = { "", "" },
-    -- 换行
-    wrap = false,
-    -- 快捷键
+    outline_window = {
+      position = "right",
+      relative_width = true,
+      width = 25,
+      auto_close = false,
+      show_numbers = false,
+      show_relative_numbers = false,
+      wrap = false,
+      show_cursorline = true,
+      hide_cursor = false,
+      jump_highlight_duration = 300,
+    },
+    outline_items = {
+      show_symbol_details = true,
+      show_symbol_lineno = false,
+      highlight_hovered_item = true,
+      auto_set_cursor = true,
+      auto_unfold_hover = true,
+    },
+    preview_window = {
+      auto_preview = false,
+      open_hover_on_preview = false,
+      width = 50,
+      min_width = 50,
+      relative_width = 0.5,
+      border = "single",
+    },
+    symbols = {
+      filter = nil,
+      icon_fetcher = nil,
+      icon_source = nil,
+    },
     keymaps = {
       close = { "<Esc>", "q" },
       goto_location = "<Cr>",
-      focus_location = "o",
+      peek_location = "o",
+      goto_and_close = "<S-Cr>",
+      restore_location = "<C-g>",
       hover_symbol = "<C-space>",
       toggle_preview = "K",
       rename_symbol = "r",
@@ -49,41 +52,46 @@ return {
       fold_all = "W",
       unfold_all = "E",
       fold_reset = "R",
+      down_and_jump = "<C-j>",
+      up_and_jump = "<C-k>",
     },
-    -- LSP 黑名单
-    lsp_blacklist = {},
-    -- 符号黑名单
-    symbol_blacklist = {},
-    -- 符号图标
+    providers = {
+      priority = { "lsp", "coc", "markdown", "norg" },
+      lsp = {
+        blacklist_clients = {},
+      },
+    },
     symbols = {
-      File = { icon = "", hl = "@text.uri" },
-      Module = { icon = "", hl = "@namespace" },
-      Namespace = { icon = "", hl = "@namespace" },
-      Package = { icon = "", hl = "@namespace" },
-      Class = { icon = "𝓒", hl = "@type" },
-      Method = { icon = "ƒ", hl = "@method" },
-      Property = { icon = "", hl = "@method" },
-      Field = { icon = "", hl = "@field" },
-      Constructor = { icon = "", hl = "@constructor" },
-      Enum = { icon = "", hl = "@type" },
-      Interface = { icon = "", hl = "@type" },
-      Function = { icon = "", hl = "@function" },
-      Variable = { icon = "", hl = "@constant" },
-      Constant = { icon = "", hl = "@constant" },
-      String = { icon = "𝓐", hl = "@string" },
-      Number = { icon = "#", hl = "@number" },
-      Boolean = { icon = "", hl = "@boolean" },
-      Array = { icon = "", hl = "@constant" },
-      Object = { icon = "", hl = "@type" },
-      Key = { icon = "🔐", hl = "@type" },
-      Null = { icon = "NULL", hl = "@type" },
-      EnumMember = { icon = "", hl = "@field" },
-      Struct = { icon = "𝓢", hl = "@type" },
-      Event = { icon = "🗲", hl = "@type" },
-      Operator = { icon = "+", hl = "@operator" },
-      TypeParameter = { icon = "𝙏", hl = "@parameter" },
-      Component = { icon = "", hl = "@function" },
-      Fragment = { icon = "", hl = "@constant" },
+      icons = {
+        File = { icon = "", hl = "Identifier" },
+        Module = { icon = "", hl = "@namespace" },
+        Namespace = { icon = "", hl = "@namespace" },
+        Package = { icon = "", hl = "@namespace" },
+        Class = { icon = "𝓒", hl = "@type" },
+        Method = { icon = "ƒ", hl = "@method" },
+        Property = { icon = "", hl = "@method" },
+        Field = { icon = "", hl = "@field" },
+        Constructor = { icon = "", hl = "@constructor" },
+        Enum = { icon = "", hl = "@type" },
+        Interface = { icon = "", hl = "@type" },
+        Function = { icon = "", hl = "@function" },
+        Variable = { icon = "", hl = "@constant" },
+        Constant = { icon = "", hl = "@constant" },
+        String = { icon = "𝓐", hl = "@string" },
+        Number = { icon = "#", hl = "@number" },
+        Boolean = { icon = "", hl = "@boolean" },
+        Array = { icon = "", hl = "@constant" },
+        Object = { icon = "", hl = "@type" },
+        Key = { icon = "🔐", hl = "@type" },
+        Null = { icon = "NULL", hl = "@type" },
+        EnumMember = { icon = "", hl = "@field" },
+        Struct = { icon = "𝓢", hl = "@type" },
+        Event = { icon = "🗲", hl = "@type" },
+        Operator = { icon = "+", hl = "@operator" },
+        TypeParameter = { icon = "𝙏", hl = "@parameter" },
+        Component = { icon = "", hl = "@function" },
+        Fragment = { icon = "", hl = "@constant" },
+      },
     },
   },
 }
