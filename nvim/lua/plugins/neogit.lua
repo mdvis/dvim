93197f0c4d94013e6e877dc9e1c742edc5462f3c
dewei.ma <xnmageis@outlook.com>
2026-04-30 18:27:09 +0800

    feat(neogit): 添加快捷键映射

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -2,5 +2,8 @@ return {
   "NeogitOrg/neogit",
   cmd = "Neogit",
   dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" },
+  },
   opts = {},
 }
